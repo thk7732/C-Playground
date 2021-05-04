72ac2a82c89abf6fc9d1c68930108ffd23fbcae4
root <root@localhost.localdomain>
2021-05-04 15:08:04 -0400

    euklid

--- src/start/hello.c
+++ src/start/hello.c
@@ -3,5 +3,23 @@
 int main()
 {
 	printf("Hello World!\n");
+	printf("Euklid!\n");
+
+	int x = 12;
+	int y= 48;
+	printf("X = %d\n", x);
+	printf("Y = %d\n", y);	
+	while(x != y)
+	{
+		if(x > y){
+		x = x - y;
+
+		} else{ 
+		y = y - x;
+		}
+	}
+
+	printf("GGT = %d\n", x);
+
 	return 0;
 }
