29dcb3140367d924e917a6db29a3ca3afaa4c32f
root <root@localhost.localdomain>
2021-05-12 13:43:19 -0400

    getchar()

--- src/start/hello.c
+++ src/start/hello.c
@@ -1,11 +1,37 @@
 #include<stdio.h>
 
+void euklid();
+void interest();
+void square(int a, int b);
+
+
+
+int main()
+{
+//interest();	
+//euklid();
+//square(20, 36);
+	int i = 0;
+while(getchar() != -1)
+{
+	i = i + 1;
+
+}
+printf("Siehe Zeichen Zahl::  %d", i);
+
+return 0;
+}
+
 void euklid(){
 	printf("Hello World!\n");
 	printf("Euklid!\n");
-
-	int x = 12;
-	int y= 48;
+	
+	int x;
+	int y;
+	printf("X= ");
+	scanf("%d", &x);
+	printf("Y= ");
+	scanf("%d", &y);
 	printf("X = %d\n", x);
 	printf("Y = %d\n", y);	
 	while(x != y)
@@ -31,7 +57,6 @@ printf("Zahl: %d  Quadrat: %d\n", start, start*start);
 }
 
 }
-
 void interest(){
 #define time  10
 #define seed_capital 1000.0
@@ -46,12 +71,3 @@ printf("Jahr: %d. Money: %f\n ", i+1, end_money);
 	
 }
 }
-int main()
-{
-interest();	
-//euklid();
-//square(20, 36);	
-return 0;
-}
-
-	
